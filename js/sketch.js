559479762a1eb08ebe5792d439641dbff77d295f
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2020-01-03 23:57:35 +0100

    test con path in sovraimpressione

--- js/sketch.js
+++ js/sketch.js
@@ -8,19 +8,21 @@ function setup() {
     capture.size(width, height);
     capture.hide();
 
+    button = createButton('submit');
+  button.position(65, height);
+//   button.mousePressed(takeSnapshot);
+
 }
 
 function draw() {
     background(0);
     pg.image(capture, 0, 0);
-
     let src = cv.imread(pg.canvas);
-    
+    let original = cv.imread(pg.canvas);
     let dst = cv.Mat.zeros(src.cols, src.rows, cv.CV_8UC3);
 
-
     cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
-    cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
+    // cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     // You can try more different parameters
@@ -29,7 +31,7 @@ function draw() {
         contours,
         hierarchy,
         cv.RETR_CCOMP,
-        cv.CHAIN_APPROX_NONE
+        cv.CHAIN_APPROX_SIMPLE
     );
     // draw contours with random Scalar
     const points = {};
@@ -41,11 +43,8 @@ function draw() {
             255,
             255
         );
-        if (area > 200) {
-            console.log(area);
-
+        if (area > 500) {
             cv.drawContours(dst, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
-
             points[i] = [];
             for (let j = 0; j < ci.data32S.length; j += 2) {
                 let p = {};
@@ -57,10 +56,31 @@ function draw() {
         }
     }
 
-    cv.imshow(canvas.canvas, dst);
+    cv.imshow(canvas.canvas, original);
+    plotPoints(canvas.canvas, points);
     src.delete();
     dst.delete();
-
+original.delete();
     contours.delete();
     hierarchy.delete();
 }
+
+function plotPoints(canvas, points){
+    const ctx = canvas.getContext('2d');
+    ctx.strokeStyle = 'green';
+  
+    Object.values(points).forEach(ps => {
+      ctx.beginPath();
+      ctx.moveTo(ps[0].x, ps[1].y);
+       ctx.arc(ps[0].x, ps[1].y, 2, 0, 2 * Math.PI)
+      ps.slice(1).forEach(({x,y})=>{
+        ctx.lineTo(x,y)
+         ctx.arc(x, y, 2, 0, 2 * Math.PI)
+      });
+      ctx.fillStyle='#ee7700';
+      ctx.fill();
+    });
+    ctx.closePath();
+    ctx.stroke();
+    ctx.clip();
+  }
