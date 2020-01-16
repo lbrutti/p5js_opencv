62a7d717c139070a5f9eac913b0594572ad399b9
Lorenzo Brutti <lorenzobrutti80@gmail.com>
2020-01-16 23:52:08 +0100

    inserito intersezioni

--- js/sketch.js
+++ js/sketch.js
@@ -1,6 +1,6 @@
-const AREA_THRESHOLD = 500;
+const AREA_THRESHOLD = 1500;
 function setup() {
-    width = 1280;
+    width = 960;
     height = 720
     canvas = createCanvas(width, height);
     canvas.id('creata');
@@ -18,6 +18,8 @@ function setup() {
     capture.hide();
     pg = createGraphics(width, height);
     background(0);
+    paper.setup(document.getElementById('creata'));
+    shapes = [];
 
 }
 
@@ -25,26 +27,18 @@ function draw() {
     background(0);
     pg.image(capture, 0, 0, width, height);
     let src = cv.imread(pg.canvas);
-
     cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
     cv.threshold(src, src, 120, 200, cv.THRESH_BINARY);
     let contours = new cv.MatVector();
     let hierarchy = new cv.Mat();
     // You can try more different parameters
-    cv.findContours(
-        src,
-        contours,
-        hierarchy,
-        cv.RETR_EXTERNAL,
-        cv.CHAIN_APPROX_NONE
-    );
+    cv.findContours(src, contours, hierarchy, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE);
     // draw contours with random Scalar
-const points = {};
+    const points = {};
     for (let i = 0; i < contours.size(); ++i) {
         const ci = contours.get(i);
         let area = cv.contourArea(ci, false);
         if (area > AREA_THRESHOLD) {
-
             let dist = cv.pointPolygonTest(ci, new cv.Point(mouseX, mouseY), true);
             if (dist > 0) { console.log(dist); }
             points[i] = [];
@@ -59,18 +53,48 @@ const points = {};
 
     }
 
-    stroke(255, 204, 255);
-    strokeWeight(5);
+    // stroke(255, 255, 255);
+    // strokeWeight(1);
+    // Object.values(points).forEach(ps => {
+    //     push(beginShape());
+    //     fill(250,250,250);
+    //     ps.slice(1).forEach(({ x, y }, i) => {
+    //         if(!(i%10)){vertex(x, y);}
+    //     });
+    //     endShape(CLOSE);
+    // });
+    // line(mouseX, 0, mouseX, height);
+    let line = new paper.Path(new paper.Point(mouseX,0));
+    line.add(mouseX,height);
+    line.strokeColor = 'black';
+
+        shapes.map(p=>p.remove());
+        line.removeOnMove();
     Object.values(points).forEach(ps => {
-        beginShape();
-        fill(255,127,0);
-        ps.slice(1).forEach(({ x, y }) => {
-            vertex(x, y);
+        let path = new paper.Path();
+        path.strokeColor = 'red';
+        ps.slice(1).forEach(({ x, y }, i) => {
+            if(!(i%30)){path.add(new paper.Point(x, y));}
         });
-        endShape(CLOSE);
+        shapes.push(path);
     });
-    line(mouseX, 0, mouseX, height);
+    paper.view.draw();
+    
+    shapes.map(p=>{
+        showIntersections(line, p);
+    })
     src.delete();
     contours.delete();
     hierarchy.delete();
+}
+
+function showIntersections(path1, path2) {
+    var intersections = path1.getIntersections(path2);
+    for (var i = 0; i < intersections.length; i++) {
+        new paper.Path.Circle({
+            center: intersections[i].point,
+            radius: 5,
+            fillColor: '#009dec'
+        }).removeOnMove();
+    }
 }
